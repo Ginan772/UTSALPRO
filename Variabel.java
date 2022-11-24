8078dbb1736f831fae571b5e8c7e730ebe150035
Ginan772 <115127583+Ginan772@users.noreply.github.com>
2022-11-24 13:15:22 +0700

    Create Variabel.java

---
+++ Variabel.java
@@ -0,0 +1,11 @@
+public class Main{
+	public static void main(String[] args) {
+        	String salam = "assalamualaikum";
+        	String nama = "Ginan Ardiansah";
+        	int lahir = 2002;
+
+        	System.out.println(salam);
+        	System.out.println("nama saya : "+nama);
+        	System.out.println("saya lahir di tahun : "+lahir);
+    }
+}
